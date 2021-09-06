d569b0ec836211240fc4fad7826c43bd009afe79
CodyxKarthi <84221126+CodyxKarthi@users.noreply.github.com>
2021-09-06 14:36:51 +0530

    TestLib

---
+++ TestLib/MathLib.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Serilog;
+
+namespace TestLib
+{
+    public class MathLib
+    {
+        private static ILogger log = Log.ForContext(typeof(MathLib));
+
+        public int Add(int a, int b)
+        {
+            log.Debug("Adding Number :" + a + ", " + b);
+            return a + b + 10;
+        }
+    }
+}
